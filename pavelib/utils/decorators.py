23c90cb0767831a2de54c77b5ce9043d4da3f49a
Stuart Young <syoung@edx.org>
2018-08-27 16:15:04 -0400

    fixup

--- pavelib/utils/decorators.py
+++ pavelib/utils/decorators.py
@@ -3,20 +3,23 @@
 import time
 from threading import Thread
 
-def timeout(function, limit=60):
+def timeout(limit=60):
     """
     kill a function if it has not completed within a specified timeframe
     """
 
-    def timed_function():
+    def timed_function(function):
 
-        function_thread = Thread(target=function)
-        function_thread.start()
+        def wrapper(limit=limit):
+            function_thread = Thread(target=function)
+            function_thread.start()
 
-        function_thread.join(float(limit))
+            function_thread.join(float(limit))
 
-        if function_thread.is_alive():
-            raise TimeoutException
+            if function_thread.is_alive():
+                raise TimeoutException
+
+        return wrapper
 
     return timed_function()
 
