31254f6a4d8cbfc6a3b3358d02dd7399fe69f9ec
Agha Awais <aghaawais01@gmail.com>
2018-08-28 09:14:23 +0000

    HTML editor bokchoy tests I

--- common/test/acceptance/pages/studio/html_component_editor.py
+++ common/test/acceptance/pages/studio/html_component_editor.py
@@ -1,5 +1,6 @@
 from common.test.acceptance.pages.studio.utils import type_in_codemirror
 from xblock_editor import XBlockEditorView
+from common.test.acceptance.pages.common.utils import click_css
 
 
 class HtmlXBlockEditorView(XBlockEditorView):
@@ -58,3 +59,44 @@ class HtmlXBlockEditorView(XBlockEditorView):
         type_in_codemirror(self, 0, content)
 
         self.q(css='.mce-foot .mce-primary').click()
+
+    def click_settings_button(self):
+        """
+        Clicks settings button on the modal
+        """
+        self.q(css='.settings-button').click()
+        self.wait_for_element_visibility('.input.setting-input[name="Editor"]', 'Editor dropdown is present')
+
+    def open_link_plugin(self):
+        self.q(css='[aria-label="Insert/edit link"]').click()
+
+    def save_static_link(self, static_link):
+        self.q(css='.mce-combobox .mce-textbox').fill(static_link)
+        self.q(css='.mce-btn.mce-primary').click()
+
+    def get_href(self):
+        self.browser.switch_to_frame(self.browser.find_element_by_tag_name('iframe'))
+        return self.q(css="#tinymce>p>a").attrs('href')[0]
+
+    def get_default_settings(self):
+        display_name_setting = self.q(css='.wrapper-comp-setting input[type="text"]:nth-child(2)').attrs('value')[0]
+        editor_setting = self.q(css='.wrapper-comp-setting .input.setting-input :nth-child(1)').text[0]
+        return [display_name_setting, editor_setting]
+
+    def get_keys(self):
+        return self.q(css='.label.setting-label[for]').text
+
+    def set_field_val(self, field_display_name, field_value):
+        """
+        If editing, set the value of a field.
+        """
+        selector = '.xblock-studio_view li.field label:contains("{}") + input'.format(field_display_name)
+        script = "$(arguments[0]).val(arguments[1]).change();"
+        self.browser.execute_script(script, selector, field_value)
+
+    def save(self):
+        """
+        Clicks save button.
+        """
+        click_css(self, '.save-button')
+
